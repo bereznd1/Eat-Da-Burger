5b7333ab71950bdf2f1dfbca479484edd9bb23aa
Dennis <bereznd1@tcnj.edu>
2018-05-06 20:21:24 -0400

    initial folder setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+var router = express.Router();
+
+
+
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hip-Hop Hamburger', false);
+
+SELECT * from burgers;
