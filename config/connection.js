99619c50f617789a26cacb7c4b9582490b2a5223
Dennis <bereznd1@tcnj.edu>
2018-05-11 19:48:20 -0400

    JawsDB info

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,22 @@
 //Sets up MySQL connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Thacarter1",
-  database: "burgers_db"
-});
+//If the server contains the JAWSDB_URL environmental variable, it connects to the JawsDB database.
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
 
-//Makes the connection
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+//If the server lacks the variable, it falls back on an explicitly defined local database.
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Thacarter1",
+    database: "burgers_db"
+  });
+}
 
+connection.connect();
 //Exports connection for our ORM to use.
 module.exports = connection;
